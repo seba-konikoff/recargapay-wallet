b19c0942383733208467ce741327f315d38edce2
sabri <seba.koni@gmail.com>
2025-08-11 11:22:03 -0300

    initial project structure

---
+++ src/main/java/ar/com/sebakoni/recargapay/walllets/WallletsApplication.java
@@ -0,0 +1,13 @@
+package ar.com.sebakoni.recargapay.walllets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallletsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallletsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/sebakoni/recargapay/walllets/WallletsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.sebakoni.recargapay.walllets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallletsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
